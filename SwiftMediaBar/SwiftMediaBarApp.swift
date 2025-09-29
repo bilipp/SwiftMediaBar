e073fd01c0bcdf3205fbe2276ef75c83858d55f7
Philipp Bischoff <p.bischoff@innoloft.com>
2025-09-29 14:15:20 +0200

    Initial Commit

---
+++ SwiftMediaBar/SwiftMediaBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMediaBarApp.swift
+//  SwiftMediaBar
+//
+//  Created by Philipp Bischoff on 29.09.25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMediaBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftMediaBarTests/SwiftMediaBarTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMediaBarTests.swift
+//  SwiftMediaBarTests
+//
+//  Created by Philipp Bischoff on 29.09.25.
+//
+
+import Testing
+@testable import SwiftMediaBar
+
+struct SwiftMediaBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
