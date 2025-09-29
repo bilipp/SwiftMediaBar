f2dd87d85d858e89cb765aa8948e163b54f41b41
Philipp Bischoff <p.bischoff@innoloft.com>
2025-09-29 14:35:14 +0200

    Refactor app structure to implement menu bar functionality and prevent termination on window close

--- SwiftMediaBar/SwiftMediaBarApp.swift
+++ SwiftMediaBar/SwiftMediaBarApp.swift
@@ -5,13 +5,41 @@
 //  Created by Philipp Bischoff on 29.09.25.
 //
 
+import AppKit
 import SwiftUI
 
 @main
 struct SwiftMediaBarApp: App {
+  @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
   var body: some Scene {
-    WindowGroup {
-      ContentView()
+    Settings {
+      EmptyView()
     }
+  }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+  private var menuBarManager: MenuBarManager?
+
+  func applicationDidFinishLaunching(_ notification: Notification) {
+    // Hide from dock and disable window creation
+    MenuBarManager.hideFromDock()
+
+    // Initialize menu bar manager
+    menuBarManager = MenuBarManager()
+    menuBarManager?.applicationDidFinishLaunching()
+
+    // Prevent the app from terminating when the last window is closed
+    NSApp.setActivationPolicy(.accessory)
+  }
+
+  func applicationWillTerminate(_ notification: Notification) {
+    menuBarManager?.applicationWillTerminate()
+  }
+
+  func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+    // Don't terminate when windows are closed - we're a menu bar app
+    return false
   }
 }
